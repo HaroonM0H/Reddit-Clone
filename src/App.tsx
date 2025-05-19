aab2ee6ae13163c1569d84886c8ffcc20c348b80
Haroon Mohammed <88782074+HaroonM0H@users.noreply.github.com>
2025-05-19 21:19:36 +0100

    refactored existing codebase to allow for easier route handling for auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 //pages
-import Home from './pages/Home'
+
+import Home from "./pages/Home"
+
 
 function App() {
   return (
-    <BrowserRouter>
+
       <div className="min-h-screen bg-zinc-900">
         <nav className="fixed top-0 left-0 right-0 z-50 bg-zinc-900 border-b border-zinc-800 shadow-lg">
           <div className="container mx-auto px-4 py-3">
@@ -13,15 +15,10 @@ function App() {
           </div>
         </nav>  
         <main className="container mx-auto px-4 pt-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* Add more routes as you create them */}
-            {/* <Route path="/login" element={<Login />} /> */}
-            {/* <Route path="/post" element={<Post />} /> */}
-          </Routes>
+          <Home />
         </main>
       </div>
-    </BrowserRouter>
+
   )
 }
 
